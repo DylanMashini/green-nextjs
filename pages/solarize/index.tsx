c38d5edbbc9eb33495bce335367a5a57e65a7f6c
DylanMashini <dylanmashini123@gmail.com>
2022-04-15 11:44:02 -0400

    feat: solarize page

--- pages/solarize/index.tsx
+++ pages/solarize/index.tsx
@@ -128,6 +128,55 @@ export default function Solarize() {
 					deserunt mollit anim id est laborum.
 				</p>
 			</div>
+			<div>
+				<h1
+					style={{
+						textAlign: "center",
+					}}
+				>
+					Pricing
+				</h1>
+				<p
+					style={{
+						marginLeft: "4vw",
+						marginRight: "4vw",
+					}}
+				>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+					do eiusmod tempor incididunt ut labore et dolore magna
+					aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+					ullamco laboris nisi ut aliquip ex ea commodo consequat.
+					Duis aute irure dolor in reprehenderit in voluptate velit
+					esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+					occaecat cupidatat non proident, sunt in culpa qui officia
+					deserunt mollit anim id est laborum.
+				</p>
+			</div>
+			<div>
+				<h1
+					style={{
+						textAlign: "center",
+					}}
+					id="impact"
+				>
+					Impacts
+				</h1>
+				<p
+					style={{
+						marginLeft: "4vw",
+						marginRight: "4vw",
+					}}
+				>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+					do eiusmod tempor incididunt ut labore et dolore magna
+					aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+					ullamco laboris nisi ut aliquip ex ea commodo consequat.
+					Duis aute irure dolor in reprehenderit in voluptate velit
+					esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+					occaecat cupidatat non proident, sunt in culpa qui officia
+					deserunt mollit anim id est laborum.
+				</p>
+			</div>
 		</div>
 	);
 }
