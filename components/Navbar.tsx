7ce01a500782671b809bfb3e88cf4383ff5cc16f
DylanMashini <dylanmashini123@gmail.com>
2022-04-15 21:03:13 -0400

    fixed overflow issue

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -170,6 +170,7 @@ export default function Navbar({ mobile }) {
 								justifyContent: "center",
 								alignItems: "center",
 								cursor: "pointer",
+								overflow: "hidden",
 							}}
 						>
 							<h4
@@ -313,6 +314,7 @@ export default function Navbar({ mobile }) {
 						justifyContent: "center",
 						alignItems: "center",
 						cursor: "pointer",
+						overflow: "hidden",
 					}}
 				>
 					<h4
