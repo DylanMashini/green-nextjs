89658e003727211c71cc461e56ec3b82603c2061
DylanMashini <dylanmashini123@gmail.com>
2022-05-18 17:07:31 -0400

    fix: website url

--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
 const isProd = process.env.NODE_ENV === "production";
 export default isProd
-	? "https://www.mge-dylanmashini.vercel.app"
+	? "https://www.mygreenearth.org"
 	: "http://localhost:3000";
