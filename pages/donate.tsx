aeb6dda07e3cab3f50aa1260888dc51b50db651f
DylanMashini <dylanmashini123@gmail.com>
2022-05-25 14:33:25 -0400

    feat: add donorbox :(

---
+++ pages/donate.tsx
@@ -0,0 +1,67 @@
+import Layout from "../components/Layout";
+import { useState, useEffect } from "react";
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Donate() {
+	const [mobile, setMobile] = useState(false);
+	const [width, setWidth] = useState(764);
+	const [height, setHeight] = useState(440);
+	const isProd = process.env.NODE_ENV === "production";
+	const getMobile = () => {
+		setWidth(window.innerWidth);
+		setHeight(window.innerHeight);
+		if (window.innerWidth < 905) {
+			setMobile(true);
+		} else {
+			setMobile(false);
+		}
+	};
+	if (!isProd) {
+		useEffect(() => {
+			getMobile();
+			window.addEventListener("resize", getMobile);
+		});
+	} else {
+		useEffect(() => {
+			getMobile();
+		});
+	}
+	return (
+		<Layout mobile={mobile}>
+			<Head>
+				<script
+					src="https://donorbox.org/widget.js"
+					// @ts-ignore
+					paypalExpress="false"
+				></script>
+				<div
+					style={{
+						display: "flex",
+						flexDirection: "column",
+						alignItems: "center",
+						justifyContent: "center",
+					}}
+				>
+					<iframe
+						src="https://donorbox.org/embed/online-donation-33"
+						name="donorbox"
+						allowpaymentrequest="allowpaymentrequest"
+						// @ts-ignore
+						seamless="seamless"
+						frameborder="0"
+						scrolling="no"
+						height="900px"
+						width="100%"
+						style={{
+							maxWidth: "500px",
+							minWidth: "250px",
+							maxHeight: "none !important",
+							marginTop: "10vh",
+						}}
+					></iframe>
+				</div>
+			</Head>
+		</Layout>
+	);
+}
