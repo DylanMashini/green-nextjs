215d98520200c76a5e713604d07eb0efdf4ff339
DylanMashini <dylanmashini123@gmail.com>
2022-07-17 15:55:03 -0400

    feat: change renderer to preact

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const withPreact = require("next-plugin-preact");
+
+const nextConfig = {
+	reactStrictMode: true,
+};
+
+module.exports = withPreact(nextConfig);
