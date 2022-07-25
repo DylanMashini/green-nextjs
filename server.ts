b3d79347bc4976b1660548066ca5a42381f4a6c8
DylanMashini <dylanmashini123@gmail.com>
2022-07-25 17:32:19 -0400

    fix: server again

--- server.ts
+++ server.ts
@@ -3,8 +3,8 @@ if (process.env.NODE_ENV === "production") {
 	server = "https://www.mygreenearth.org";
 } else if (process.env.NODE_ENV === "development") {
 	server = "http://localhost:3000";
-} else {
-	server = `https://${process.env.VERCEL_URL}`;
+} else if (typeof window !== "undefined") {
+	server = window.location.origin;
 }
 
 export default server;
