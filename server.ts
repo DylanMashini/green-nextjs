adbf97aabb512b2bc49b561457db40d73388b873
DylanMashini <dylanmashini123@gmail.com>
2022-07-25 17:30:22 -0400

    fix server??

--- server.ts
+++ server.ts
@@ -1,3 +1,10 @@
-export default process.env.VERCEL_URL
-	? `https://${process.env.VERCEL_URL}`
-	: "http://localhost:3000";
+let server;
+if (process.env.NODE_ENV === "production") {
+	server = "https://www.mygreenearth.org";
+} else if (process.env.NODE_ENV === "development") {
+	server = "http://localhost:3000";
+} else {
+	server = `https://${process.env.VERCEL_URL}`;
+}
+
+export default server;
