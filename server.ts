601b5f5bec28e392d9c58599839f79d486cecb04
DylanMashini <54782808+DylanMashini@users.noreply.github.com>
2022-08-01 20:31:24 -0400

    fix: server

--- server.ts
+++ server.ts
@@ -2,6 +2,8 @@ let server;
 
 if (process.env.NODE_ENV === "development") {
 	server = "http://localhost:3000";
+} else if (process.env.IS_PROD === "TRUE") {
+	server = `https://www.mygreenearth.org`
 } else if (process.env.VERCEL_URL) {
 	server = `https://${process.env.VERCEL_URL}`;
 } else if (typeof window !== "undefined") {
