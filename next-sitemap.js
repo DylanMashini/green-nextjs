26931c99947bf702353b40657d8d96564533628e
DylanMashini <54782808+DylanMashini@users.noreply.github.com>
2023-01-09 09:30:39 -0500

    Fix: Remove admin pages from sitemap

--- next-sitemap.js
+++ next-sitemap.js
@@ -3,11 +3,5 @@
 module.exports = {
 	siteUrl: process.env.SITE_URL || "https://www.mygreenearth.org/",
 	generateRobotsTxt: true,
-	exclude: [
-		"/admin/login",
-		"/admin",
-		"/admin/register",
-		"/admin/event",
-		"/admin/event/new",
-	],
+	exclude: ["/admin/*"],
 };
